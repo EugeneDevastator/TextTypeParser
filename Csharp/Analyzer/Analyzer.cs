55db7ecc6c2be0af7b240f40694e54ec71809ff1
EugeneDevastator <omnispark@gmail.com>
2022-10-19 18:32:56 +0200

    update algo to scan for better pos

--- Csharp/Analyzer/Analyzer.cs
+++ Csharp/Analyzer/Analyzer.cs
@@ -11,6 +11,7 @@ public class Analyzer
     private NDArray counts;
 
     private Dictionary<char, byte> typIndices = new Dictionary<char, byte>();
+    private CharData.CharData _charData;
 
     public Analyzer()
     {
@@ -21,6 +22,7 @@ public class Analyzer
         {
             typIndices[Constants.typable[i]] = (byte)i;
         }
+        _charData = new CharData.CharData();
     }
 
     public void GenerateLayout()
@@ -30,11 +32,11 @@ public class Analyzer
         char[,] chars = new char[11, 3];
         byte[,] priority = new byte[11, 3];
         int[,] lCounts = new int[11, 3];
-        
+
         //scan ranges for next cell detection and nearby detection
         int yrange = 2;
-        int xrange = 2;
-        
+        int xrange = 1;
+
         bool IsValid(int x, int y) => x >= 0 && x < 11 && y >= 0 && y < 3 && chars[x, y] != SKIP;
 
         bool IsValidChar(int x, int y) =>
@@ -53,6 +55,7 @@ public class Analyzer
                     }
                 }
             }
+
             w += priority[x, y] * 100000000;
             return w;
         }
@@ -64,7 +67,8 @@ public class Analyzer
             {
                 for (int k = -yrange; k <= yrange; k++)
                 {
-                    if (IsValidChar(x + i, y + k))
+                    //k!=0 will take horizonatals out of calculations.
+                    if (IsValidChar(x + i, y + k) && k != 0)
                     {
                         res.Append(chars[x + i, y + k]);
                     }
@@ -80,21 +84,24 @@ public class Analyzer
         {
             "*****_*****",
             "arst*_*neio",
-            ",****_****.",
-        };
-        
-        string[] prio = new string[3]
-        {
-            "34552025543",
-            "00003_30000",
-            "03331_13330",
+            "*****_*****",
         };
 
-        
+        string[] prio = new string[3]
+        {
+            //  "34552025543",
+            //  "00003_30000",
+            //  "54331_13345",
+            "27883_38872",
+            "99995_59999",
+            "24580_08542",
+        };
+//there is one more problem that we will fill one half first, due to increased entropy after first key is placed.
+
         string unuse = " ";
         foreach (var c in unuse)
         {
-            unparsedYet=unparsedYet.Replace(c.ToString(),String.Empty);
+            unparsedYet = unparsedYet.Replace(c.ToString(), String.Empty);
         }
 
         // Initial Fill, dont want to index arrays in row-column format.
@@ -108,60 +115,151 @@ public class Analyzer
                 if (c != SKIP && c != NONE)
                 {
                     lCounts[i, k] = counts[typIndices[c]];
-                    unparsedYet=unparsedYet.Replace(c.ToString(),String.Empty);
+                    unparsedYet = unparsedYet.Replace(c.ToString(), String.Empty);
                 }
                 else
                     lCounts[i, k] = 0;
             }
         }
 
-        while (!String.IsNullOrEmpty(unparsedYet))
+        // new algo:
+        // 1. pick most used key
+        // 1.2 get key locations for most prioritized batch;
+        // 2. find location with least interactions for new key inside the batch.
+        List<(int x, int y)> GetCandidatePositions()
         {
-
-            int nextx = -1;
-            int nexty = -1;
-            int maxVal = 0;
-            for (int k = 0; k < 3; k++)
+            var maxBatch = 0;
+            foreach (var coord in chars.Coords())
             {
-                for (int i = 0; i < 11; i++)
+                if (chars[coord.x, coord.y] == NONE)
+                    if (priority[coord.x, coord.y] is var p && p > maxBatch)
+                        maxBatch = p;
+            }
+
+            List<(int x, int y)> batchCoords = new List<(int x, int y)>();
+            foreach (var coord in chars.Coords())
+            {
+                if (chars[coord.x, coord.y] == NONE)
+                    if (priority[coord.x, coord.y] is var p && p == maxBatch)
+                        batchCoords.Add(coord);
+            }
+
+            return batchCoords;
+        }
+
+        void GenerateLayoutByLetterAndBatches()
+        {
+            while (!String.IsNullOrEmpty(unparsedYet))
+            {
+                var maxCount = 0;
+                char nextC = '\0';
+                foreach (var c in unparsedYet.ToCharArray())
                 {
-                    if (chars[i, k] !=  NONE)
-                        continue;
-                    
-                    var w = CellWeight(i, k);
-                    if (w > maxVal)
+                    if ((int)counts[typIndices[c]] is var cnt && cnt > maxCount)
                     {
-                        maxVal = w;
-                        nextx = i;
-                        nexty = k;
+                        maxCount = cnt;
+                        nextC = c;
                     }
                 }
-            }
 
-            if (nextx < 0 || nexty < 0)
-                break;
-            
-            var matchStatic = GetNearChars(nextx, nexty);
-            var mostFit = LessAdjacentForAllWMetric(matchStatic, unparsedYet);
-            unparsedYet= unparsedYet.Replace(mostFit.ToString(),String.Empty);
-            chars[nextx, nexty] = mostFit;
-            lCounts[nextx, nexty] = counts[typIndices[mostFit]];
+                var batch = GetCandidatePositions();
+                var bestcoord = batch[0];
+                float maxRate = 0;
+                foreach (var coord in batch)
+                {
+                    var matchStatic = GetNearChars(coord.x, coord.y);
+                    //return count as well.
+                    var (selfchar, weight) = LessAdjacentForAllWMetric(matchStatic, nextC.ToString());
+                    if (weight > maxRate)
+                    {
+                        maxRate = weight;
+                        bestcoord = coord;
+                    }
+                }
+
+                chars[bestcoord.x, bestcoord.y] = nextC;
+                lCounts[bestcoord.x, bestcoord.y] = counts[typIndices[nextC]];
+                unparsedYet = unparsedYet.Replace(nextC.ToString(), String.Empty);
+            }
         }
 
-        for (int k = 0; k < 3; k++)
+        void GenerateLayoutByEmptyWeightFirst()
         {
-            string line = "";
-            for (int i = 0; i < 11; i++)
+            while (!String.IsNullOrEmpty(unparsedYet))
             {
-                line += chars[i, k];
+                int nextx = -1;
+                int nexty = -1;
+                int maxVal = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        if (chars[i, k] != NONE)
+                            continue;
+
+                        var w = CellWeight(i, k);
+                        if (w > maxVal)
+                        {
+                            maxVal = w;
+                            nextx = i;
+                            nexty = k;
+                        }
+                    }
+                }
+
+                if (nextx < 0 || nexty < 0)
+                    break;
+
+                var matchStatic = GetNearChars(nextx, nexty);
+                var (mostFit, _) = LessAdjacentForAllWMetric(matchStatic, unparsedYet);
+                unparsedYet = unparsedYet.Replace(mostFit.ToString(), String.Empty);
+                chars[nextx, nexty] = mostFit;
+                lCounts[nextx, nexty] = counts[typIndices[mostFit]];
             }
-            Console.WriteLine(line);
         }
-        Console.WriteLine("leftover:"+unparsedYet);
+
+        void PrintForTable()
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                string line = "";
+                for (int i = 0; i < 11; i++)
+                {
+                    line += _charData.NameOf(chars[i, k]) + " ";
+                }
+
+                Console.WriteLine(line);
+            }
+        }
+
+        void PrintRaw()
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                string line = "";
+                for (int i = 0; i < 11; i++)
+                {
+                    line += chars[i, k];
+                }
+
+                Console.WriteLine(line);
+            }
+        }
+        
+        
+
+        //GenerateLayoutByEmptyWeightFirst();
+        GenerateLayoutByLetterAndBatches();
+        PrintRaw();
+        Console.WriteLine();
+        PrintForTable();
+
+        Console.WriteLine("leftover:" + unparsedYet);
     }
 
 
-    public char LessAdjacentForAllWMetric(string setStatic, string setLook)
+    //bigger the number = lower the adjacency rating over count.
+    public (char, float) LessAdjacentForAllWMetric(string setStatic, string setLook)
     {
         var staticChars = setStatic.ToCharArray();
         var lookChars = setLook.ToCharArray();
@@ -179,7 +277,7 @@ public class Analyzer
             }
 
             var cnt = counts.GetInt64(lookCharIds[i]);
-            sumCounts[i] = (cnt*cnt)/ sumCounts[i];
+            sumCounts[i] = (cnt * cnt) / sumCounts[i];
         }
 
         var sortedSumIds = np.asarray(sumCounts).argsort<float>();
@@ -188,10 +286,11 @@ public class Analyzer
         Console.WriteLine("adjacency sums to: " + setStatic);
         Console.WriteLine(sortedSums.ToString());
         Console.WriteLine(sortedChars);
-        return sortedChars[^1]; //getting max;
+        Console.WriteLine("last sum:" + sortedSums.GetSingle(-1));
+        return (sortedChars[^1], sortedSums.GetSingle(-1)); //getting max;
     }
-    
-    public char LessAdjacentForAll(string setStatic, string setLook)
+
+    public (char, float) LessAdjacentForAll(string setStatic, string setLook)
     {
         var staticChars = setStatic.ToCharArray();
         var lookChars = setLook.ToCharArray();
@@ -215,7 +314,7 @@ public class Analyzer
         Console.WriteLine("adjacency sums to: " + setStatic);
         Console.WriteLine(sortedSums.ToString());
         Console.WriteLine(sortedChars);
-        return sortedChars[0];
+        return (sortedChars[0], sortedSums.GetSingle(0));
     }
 
     //find sorted char adjacencies to each one from static set.

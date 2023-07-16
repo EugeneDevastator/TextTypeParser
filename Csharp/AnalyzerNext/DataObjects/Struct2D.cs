81e733ff122cab5a3795186548fc8754f748b486
EugeneDevastator <omnispark@gmail.com>
2023-07-16 10:28:26 +0200

    cleanup

--- Csharp/AnalyzerNext/DataObjects/Struct2D.cs
+++ Csharp/AnalyzerNext/DataObjects/Struct2D.cs
@@ -6,16 +6,10 @@ namespace AnalyzerNext;
 public class Struct2D<T> where T : struct, IEquatable<T>
 {
     private readonly T[,] _data;
-    private readonly (byte x, byte y)[] _flatCoords;
-
-    public T[,] Data => _data;
-    public byte Xdim => (byte)_data.GetLength(0);
-    public byte Ydim => (byte)_data.GetLength(1);
-    public (byte x, byte y)[] CoordsArray => _flatCoords;
-    public List<T> Flatten => CoordsArray.Select(pos => this[pos]).ToList();
+    private readonly (ushort x, ushort y)[] _flatCoords;
 
 
-    public Struct2D(int width, int height)
+    public Struct2D(ushort width, ushort height)
     {
         _data = new T[width, height];
         _flatCoords = GenerateCoords().ToArray();
@@ -27,11 +21,54 @@ public class Struct2D<T> where T : struct, IEquatable<T>
             _data[x, y] = other[x, y];
     }
 
-    private IEnumerable<(byte x, byte y)> GenerateCoords()
+    public T[,] Data => _data;
+    public ushort Xdim => (ushort)_data.GetLength(0);
+    public ushort Ydim => (ushort)_data.GetLength(1);
+    public (ushort x, ushort y)[] CoordsArray => _flatCoords;
+    public List<T> Flatten => CoordsArray.Select(pos => this[pos]).ToList();
+
+    public T this[int x, int y]
     {
-        for (byte i = 0; i < Xdim; i++)
+        get => _data[x, y];
+        set => _data[x, y] = value;
+    }
+
+    public T this[KeyCoord c]      
+    {
+        get => this[c.x, c.y];
+        set => this[c.x, c.y] = value;
+    }
+
+    public T this[(ushort x, ushort y) c]     
+    {
+        get => this[c.x, c.y];
+        set => this[c.x, c.y] = value;
+    }
+
+    public static Struct2D<T> Map<TRead>((ushort x, ushort y) end,Func<int,int,TRead> getter, Func<TRead,T> mapper)
+    {
+        return Map((0, 0), end, getter, mapper);
+    }
+
+    public static Struct2D<T> Map<TRead>((ushort x, ushort y) start, (ushort x, ushort y) end, Func<int, int, TRead> getter, Func<TRead, T> mapper)
+    {
+        (ushort x, ushort y) dims = ((ushort)(end.x - start.x + 1), (ushort)(end.y - start.y + 1));
+        var s2d = new Struct2D<T>(dims.x, dims.y);
+        for (ushort i = 0; i < dims.x; i++)
         {
-            for (byte k = 0; k < Ydim; k++)
+            for (ushort k = 0; k < dims.y; k++)
+            {
+                s2d[i, k] = mapper(getter(start.x + i, start.y + k));
+            }
+        }
+        return s2d;
+    }
+
+    private IEnumerable<(ushort x, ushort y)> GenerateCoords()
+    {
+        for (ushort i = 0; i < Xdim; i++)
+        {
+            for (ushort k = 0; k < Ydim; k++)
             {
                 yield return (i, k);
             }
@@ -46,23 +83,7 @@ public class Struct2D<T> where T : struct, IEquatable<T>
             _data[x, y] = other[x, y];
         }
     }
-    
-    public T this[int x, int y]
-    {
-        get => _data[x, y];
-        set => _data[x, y] = value;
-    }
-    public T this[KeyCoord c]      
-    {
-        get => this[c.x, c.y];
-        set => this[c.x, c.y] = value;
-    }
-    public T this[(byte x, byte y) c]     
-    {
-        get => this[c.x, c.y];
-        set => this[c.x, c.y] = value;
-    }
-    
+
     public void UpdateEach(Func<T, T> updater)
     {
         foreach (var (x,y) in CoordsArray)
@@ -70,7 +91,7 @@ public class Struct2D<T> where T : struct, IEquatable<T>
             _data[x, y] = updater(_data[x, y]);
         }
     }
-    
+
     public void ReplaceManyWithOne(T[] find, T replace)
     {
         UpdateEach(c=> find.Contains(c) ? replace : c);
@@ -80,7 +101,7 @@ public class Struct2D<T> where T : struct, IEquatable<T>
     {
         UpdateEach(c=> c.Equals(find) ? replace : c);
     }
-    
+
     public override string ToString()
     {
         return new string(ToStringList().SelectMany(e => (e + "\n").ToCharArray()).ToArray());
@@ -104,7 +125,4 @@ public class Struct2D<T> where T : struct, IEquatable<T>
 
         return output;
     }
-    
-    
-    
 }
